f5afb76d3aaf1b04e0eb2c7b46a091f1f220fd3e
Rakshak <rakshakchoudhary14@gmail.com>
2024-05-09 18:23:53 +0530

    my commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -99,10 +99,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -162,4 +162,5 @@ REST_FRAMEWORK = {
 }
 import os
 # STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+#STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')

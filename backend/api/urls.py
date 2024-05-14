81b99a1aee651c8dce4368609a459372f136ac34
Rakshak <rakshakchoudhary14@gmail.com>
2024-05-14 16:49:14 +0530

    Moanday

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,14 +1,14 @@
 # api/urls.py
 
 from django.urls import path
-from .views import RegistrationView, LoginView, UploadVideoView,StartInterviewView, ContactCreateView 
-# , ResetPasswordView
+from .views import RegistrationView, LoginView, UploadVideoView, ContactCreateView 
+# , ResetPasswordView, StartInterviewView
 
 urlpatterns = [
     path("register", RegistrationView.as_view(), name="register"),
     path("login", LoginView.as_view(), name="login"),
     path("video", UploadVideoView.as_view(), name="video"),
-    path("start-interview/", StartInterviewView.as_view(), name="start-interview/"),
+    # path("start-interview/", StartInterviewView.as_view(), name="start-interview/"),
     path("contact_us/", ContactCreateView.as_view(), name="contact_us/"),
     #path("forgotPassword", ForgotPasswordView.as_view(), name="forgotPassword"),
     # path("resetPassword", ResetPasswordView.as_view(), name="resetPassword"),
